4c45d4a4b70bdcd5b21736a94efd5f09ea056265
erkanina <erkan_ermis@hotmail.com>
2022-06-19 15:48:18 +0300

    Bidirectional

--- publisher.js
+++ publisher.js
@@ -4,14 +4,31 @@ const client = mqtt.connect('mqtt://localhost');
 
 var counter = 0;
 
+client.on('connect', () => {
+    console.log('Publisher - Connected to broker');   
+    
+    client.subscribe('mqtt/SetCounter');
+  
+    console.log('Publisher - Subscribed'); 
+  });
+
+client.on('message', (topic, payload) => {
+    switch(topic){
+        case 'mqtt/SetCounter':                        
+            counter = 0;
+            console.log('Publisher - Rx Set Counter : ');
+        break;
+    }
+});
+
 function publishData() {
     client.publish(
-        "mqtt/myCounter",
+        "mqtt/Counter",
         JSON.stringify(`Timer:${counter}`)
       );
 
-      console.log("Published : " + counter);
-      
+      console.log("Publisher - Published : " + counter);
+
       counter++;
 }
   
--- subscriber.js
+++ subscriber.js
@@ -2,16 +2,42 @@ const mqtt = require('mqtt')
 const client = mqtt.connect('mqtt://localhost')
 
 client.on('connect', () => {
-  console.log('Connected to broker');
+  console.log('Subscriber - Connected to broker');
   
-  client.subscribe('mqtt/myCounter');
-  console.log('Subscribed to \"mqtt/myCounter\"'); 
+  client.subscribe('mqtt/Counter');
+  client.subscribe('mqtt/SetCounter');
+
+  console.log('Subscriber - Subscribed'); 
 });
 
 client.on('message', (topic, payload) => {
     switch(topic){
-        case 'mqtt/myCounter':
-            console.log('Rx message : ' + payload.toString());
-        break;        
+        case 'mqtt/Counter':
+            // 1. Direkt yaz
+            console.log('Subscriber - Rx message : ' + payload.toString());
+
+            //.. JSON'a çevirip yaz
+            if(payload) {
+                let payloadString = payload.toString();
+                try {
+                    let payloadJson =  JSON.parse(payloadString);
+                    console.log(payloadJson);
+                } catch (error) {
+                    console.log("Invalid json data");
+                }
+            }           
+
+        break;       
     }    
 });
+
+function SetCounter() {
+    client.publish(
+        "mqtt/SetCounter",
+        JSON.stringify(`Timer:${0}`)
+      );
+
+      console.log('Subscriber - Set Counter  0');
+}
+
+setInterval(SetCounter, 15000);
